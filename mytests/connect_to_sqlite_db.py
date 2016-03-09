a395126940869153fed89881bba406f9da76f473
Liorinco <loic_tangre9@hotmail.fr>
2016-03-09 17:14:48 +0100

    Fix: Correct db file path

--- mytests/connect_to_sqlite_db.py
+++ mytests/connect_to_sqlite_db.py
@@ -1,16 +1,17 @@
 # -*- coding: utf8 -*-
 
-"""
-    !!! FAIL: Never run !!!
-"""
-
 import os.path
 import sqlite3
 
 
-project_path = os.path.abspath(os.pardir)
-conn = sqlite3.connect(
-    os.path.abspath(os.path.join(project_path, "mysite", "db.sqlite3")))
+project_path = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+db_path = os.path.abspath(os.path.join(project_path, "mysite", "db.sqlite3"))
+conn = sqlite3.connect(db_path)
 curs = conn.cursor()
 curs.execute("PRAGMA database_list;")
+for result in curs:
+    print result
+curs.execute("SELECT name FROM main.sqlite_master WHERE type='table';")
+for result in curs:
+    print result
 conn.close()
